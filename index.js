b24856e75da4641d8ad28a048ee3be1d1cc76bf8
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2015-04-02 18:28:16 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dc: 'http://purl.org/dc/terms/',
+  foaf: 'http://xmlns.com/foaf/0.1/',
+  owl: 'http://www.w3.org/2002/07/owl#',
+  rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+  rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
+  rr: 'http://www.w3.org/ns/r2rml#',
+  skos: 'http://www.w3.org/2004/02/skos/core#',
+};
