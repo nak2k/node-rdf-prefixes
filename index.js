25adb754b03f20b573800abbbbcbf9dedc5e45eb
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2015-04-04 12:01:47 +0900

    Modify dc: and add dcterms:

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
-  dc: 'http://purl.org/dc/terms/',
+  dc: 'http://purl.org/dc/elements/1.1/',
+  dcterms: 'http://purl.org/dc/terms/',
   foaf: 'http://xmlns.com/foaf/0.1/',
   owl: 'http://www.w3.org/2002/07/owl#',
   rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
