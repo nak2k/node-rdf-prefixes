2a6cb96f3a1bcc7dfdab2503656fef7dd25145f2
Kengo Nakatsuka <kengo.nakatsuka@gmail.com>
2015-12-18 12:29:41 +0900

    Add oa:

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
   dcterms: 'http://purl.org/dc/terms/',
   foaf: 'http://xmlns.com/foaf/0.1/',
   ldp: 'http://www.w3.org/ns/ldp#',
+  oa: 'http://www.w3.org/ns/oa#',
   owl: 'http://www.w3.org/2002/07/owl#',
   rdf: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
   rdfs: 'http://www.w3.org/2000/01/rdf-schema#',
